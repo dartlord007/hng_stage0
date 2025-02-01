eda196af7176603f27ace15b70a0b7f6d94c5319
Kolade <kolademodupe007@gmail.com>
2025-02-01 16:03:13 +0100

    views

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import get_info
+from api import views
 
 urlpatterns = [
-    path('api/', get_info, name='get_info'),
+    # path('api/', get_info, name='get_info'),
+     path("", views.get_info, name="get_info"),
 ]
